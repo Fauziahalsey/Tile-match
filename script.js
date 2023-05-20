d479629fc4d3feff4dfe1678efd81e6b85c17de8
Fauziahalsey <fauziaomala19@gmail.com>
2023-05-21 01:20:48 +0300

    creating a modal

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-const time = document.getElementsByClassName(".time");
-const moves = document.getElementsById("#moves");
-const stopbtn = document.getElementsById("#stopbtn");
-const startbtn = document.getElementsById("#startbtn");
-const start = document.getElementsByClassName("start")
 
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  };
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+  openModalBtn.addEventListener("click", openModal);
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+  const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+  closeModalBtn.addEventListener("click", closeModal);
+
+  overlay.addEventListener("click", closeModal);
+
+  document.addEventListener("keydown");
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+      modalClose();
+    }
+  })
