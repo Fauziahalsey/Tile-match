e069c5ede396e68ddf062b7932b5cf36c5ffb2f3
Fauziahalsey <fauziaomala19@gmail.com>
2023-05-20 22:43:15 +0300

    aligning

---
+++ script.js
@@ -0,0 +1,9 @@
+const time = document.getElementsByClassName(".time");
+const moves = document.getElementsById("#moves");
+const stopbtn = document.getElementsById("#stopbtn");
+const startbtn = document.getElementsById("#startbtn");
+const start = document.getElementsByClassName("start")
+
+//START BUTTON
+
+startbtn.add
