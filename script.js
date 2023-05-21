bd801f581e1866087581e1faf40fc162a83a3fe5
Fauziahalsey <fauziaomala19@gmail.com>
2023-05-21 10:51:08 +0300

    flipping

--- script.js
+++ script.js
@@ -40,3 +40,13 @@ const openModal = function () {
   var memory_values = [];
   var memory_tile_ids =[];
   var tiles_flpped = 0;
+  Array.prototype.memory_tile_shuffle = function(){
+    var i = this.length, j, temp;
+    while(--i > 0){
+    j = Math.floor(Math.random () * (i+1));
+    temp =this[j];
+    this[j] = this[i];
+    this[i] = temp;
+    
+  }  
+}
