dbb4642c80a99b81f6ab124d360e8436477b7c9b
Fauziahalsey <fauziaomala19@gmail.com>
2023-05-21 18:13:25 +0300

    styling

--- script.js
+++ script.js
@@ -38,9 +38,9 @@ const openModal = function () {
 
   var memory_array = ['banana', 'banana', 'peach', 'peach', 'mango', 'mango', 'guava',',']
   var memory_values = [];
-  var memory_tile_ids =[];
-  var tiles_flpped = 0;
-  Array.prototype.memory_tile_shuffle = function(){
+  var memory_Box_ids =[];
+  var Box_flipped = 0;
+  Array.prototype.memory_Box_shuffle = function(){
     var i = this.length, j, temp;
     while(--i > 0){
     j = Math.floor(Math.random () * (i+1));
@@ -52,11 +52,53 @@ const openModal = function () {
 }
 
 function newBoard(){
-    tiles_flpped = 0;
+    Box_flipped = 0;
     var output = '';
-    memory_array.memory_tile_shuffle();
+    memory_array.memory_Box_shuffle();
     for (var i = 0; i < memory_array.length; i++){
-        output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+        output += '<div id="Box_'+i+'" onclick="memoryFlipBox(this,\''+memory_array[i]+'\')"></div>';
     }
     document.getElementById('memory_board').innerHTML = output;
+}
+
+function memoryFlipBox(Box,val){
+    if(tile.innerHTML==""&& memory_values < 2){
+        box.style.background = '#fff';
+        box.innerHTML = val;
+        if(memory_values.length == 0){
+            memory_values.push(val);
+            memory_Box_ids.push(Box.id);
+            
+        } else if (memory_values.length == 1){
+            memory_values.push(val);
+            memory_Box_ids.push(Box.id);
+            if(memory_values[0] ==memory_values[1]){
+                Box_flipped +=2;
+
+                memory_values = [];
+                memory_Box_ids = [];
+                if(Box_flpped == memory_array.length){
+                    alert("Board cleared...generating new board");
+                    document.getElementById('memory_board').innerHTML = "";
+                    newBoard();
+                }
+
+            } else {
+                function flip2Back (){
+                    var Box_1 = document.getElementById(memory_Box_ids[0]);
+                    var Box_2 = document.getElementById(memory_Box_ids[1]);
+                    tile_1.style.background = 'url(star_bg.ppg) no repeat'
+                    tile_1.innerHTML = "";
+                    tile_2.style.background = 'url(star_bg.png) no repeat'
+                    tile_2.innerHTML = "";
+
+                    memory_values = [];
+                    memory_Box_ids = [];
+
+
+                }
+                setTimeout(flip2Back,700);
+            }
+        }
+    }
 }
